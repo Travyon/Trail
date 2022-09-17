c6c29d053ae36271a1a2a2924e701e9cdf9902d6
Travyon <treystvns05@gmail.com>
2022-09-17 15:26:46 -0500

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, ImageBackground} from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    
+      <View style={styles.titleContainer}>
+        <ImageBackground 
+          source={require('./assets/Images/Trail-Background.jpeg')}
+          style={styles.image}
+        />
+
+        <View style={styles.titles}>
+         <Text style={styles.title}>Trail</Text>
+         <Text style={styles.subtitle}>Your path to financial freedom</Text>
+        </View>
+
+
+      </View>
+
     </View>
   );
 }
@@ -17,4 +33,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  titleContainer:{
+    width: '100%',
+    height: '100%'
+  },
+  
+  titles: {
+    marginTop:'30%',
+    width:'100%',
+    alignItems: 'center'
+  },
+  title:{
+    fontSize: '40px',
+    fontWeight: '700',
+    color: '#FFFFFF'
+  },
+  subtitle:{
+    fontSize: '16px',
+    color: '#FFFFFF'
+  },
+
+  image:{
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+    position: 'absolute'
+  }
+
 });
