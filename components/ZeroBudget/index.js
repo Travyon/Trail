10074b17058e3a87490b3266436fb3b65985520b
Travyon <treystvns05@gmail.com>
2022-11-10 23:04:26 -0600

    Redesigned zero-based budget screen

--- components/ZeroBudget/index.js
+++ components/ZeroBudget/index.js
@@ -1,23 +1,46 @@
 import React from "react";
-import { View, Text, im} from "react-native";
-import { List, Divider, Card, Surface} from "react-native-paper";
+import { View, Text, ImageBackground} from "react-native";
+import { List, Divider, Card, Surface, FAB} from "react-native-paper";
 import styles from "./styles";
 
 
+
 const ZeroBudget =()=>{
+
+
     return(
-        <View>
-        <Text style={styles.title}>Zero-Based Budget</Text>
-        <Surface elevation={4} style={{height: 100, marginLeft: 30, marginRight: 30, marginTop: 30, }}>
-            <Text style={{textAlign: 'center', marginTop: 30, fontSize: 30, fontWeight: '500'}}>$100</Text>
+        <View style={styles.image} >
+            <ImageBackground 
+            source={require('../../assets/Images/Trailback.jpeg')}
+            style={styles.image}
+            blurRadius={150}
+          />
+
+        <Surface elevation={5} style={{height: 110, marginLeft: 30, marginRight: 30, marginTop: 100, borderRadius: 20, backgroundColor: 'black'}}>
+            <Text style={{textAlign: 'center',color: 'gold', marginTop: 10, fontWeight: '600'}}>Remaining Balance</Text>
+            <Text style={{textAlign: 'center', marginTop: 15, fontSize: 30, fontWeight: '700', color: 'gold'}}>$100</Text>
         </Surface>
-        <Card style={{marginTop: 90, padding: -53}}>
+
+        <Surface elevation={5} style={{height: 110, marginTop: 50, borderRadius: 20, backgroundColor:'black', shadowOpacity: 1, opacity: 10}}>
+            <Text style={{textAlign: 'center', marginRight: 305, fontSize: 25, fontWeight: '700', marginTop: 10, color: 'gold' }}>Bills:</Text>
             <List.Item
-                titleStyle={{marginTop:20}}
+                titleStyle={{marginTop:-5, color: 'white'}}
+                descriptionStyle={{marginTop:1}}
+                title="AT&T"
+                description="$150"
+            />
+
+        </Surface>
+       
+
+        <Surface style={{marginTop: 50, padding: -53, backgroundColor:'white', borderRadius: 30}} elevation={5} >
+            <List.Item
+                titleStyle={{marginTop:10, fontSize: 18}}
                 descriptionStyle={{marginTop: 0}}
                 title="First Item"
-                description="Item description"
+                description="Item description"  
             />
+       
             <Divider/>
             <List.Item
                 titleStyle={{marginTop:-5}}
@@ -33,22 +56,15 @@ const ZeroBudget =()=>{
                 title="Third Item"
                 description="Item description"
             />
-            <Divider/>
-            <List.Item
-                titleStyle={{marginTop:-5}}
-                descriptionStyle={{marginTop: 0}}
-                title="Fourth Item"
-                description="Item description"
-            />
-            <Divider/>
-             <List.Item
-                titleStyle={{marginTop:-5}}
-                descriptionStyle={{marginTop: 0}}
-                title="Fifth Item"
-                description="Item description"
-            />
-        </Card>
-            
+
+
+        </Surface>
+        <FAB
+        icon= "plus"
+        style={{ marginTop: 120, marginRight: 50, marginLeft:285, marginBottom: 20, backgroundColor: 'gold'}}
+        onPress={()=> console.log('Pressed')}
+        />
+   
         </View>
 
     )
--- components/ZeroBudget/styles.js
+++ components/ZeroBudget/styles.js
@@ -1,12 +1,21 @@
 import { StyleSheet } from "react-native";
 
 
+
 const styles = StyleSheet.create({
 
 title:{
    marginTop: 75,
    textAlign: 'center',
-   fontSize: 30  
+   fontSize: 30,
+   color: 'white'
+},
+image:{
+   width: '100%',
+   height: '100%',
+   resizeMode: 'stretch',
+   position: 'absolute',
+
 }
 
 })
